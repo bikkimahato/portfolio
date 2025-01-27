d6e7a1cae457ed5e7ced7ddc7320360efcf32958
bikkimahato <bikkimahato123@gmail.com>
2025-01-27 23:21:51 +0530

    update aboutData

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
   title: "Who I am",
-  description1: `Energetic Software Engineer with 3+ years experience developing robust code for high-volume businesses.
+  description1: `Energetic Software Engineer with 5.6 years experience developing robust code for high-volume businesses.
     An enthusiastic team player and a deep creative thinker.`,
   description2: `I’m familiar with a variety of programming languages and technologies, including  JavaScript, Java, Spring, Spring Boot, HTML, CSS, ReactJS, NextJS, MySQL and MongoDB but I’m always adding new skills to my repertoire. I’m also eager to meet other people in the area, so feel free to connect!`,
   image: 1,
